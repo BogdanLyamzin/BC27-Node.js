ac0a01528563dfc71432ea937665d7b525825dbf
unknown <bogdan.lyamzin.d@gmail.com>
2022-12-13 13:10:41 +0200

    finish lesson 9

---
+++ lesson-9/auth-example/controllers/auth/register.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs")
+const gravatar = require("gravatar")
+
+const {User} = require("../../models/user")
+
+const {HttpError} = require("../../helpers")
+
+const register = async(req, res)=> {
+    const {name, email, password} = req.body;
+    const user = await User.findOne({email});
+    if(user) {
+        throw HttpError(409, "Email in use")
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const newUser = await User.create({name, email, password: hashPassword, avatarURL});
+
+    res.status(201).json({
+        name: newUser.name,
+        email: newUser.email,
+    })  
+}
+
+module.exports = register;
